742873013f2ef6a16ccea5f650d18d37828c7e72
atif waqar <atif.sheikh87@gmail.com>
2025-08-12 11:04:16 +0200

    Defer Google Ads script until user interaction

--- js/ads.js
+++ js/ads.js
@@ -1,14 +1,21 @@
 (function(){
+  var loaded=false;
   function loadAds(){
+    if(loaded) return;
+    loaded=true;
     var s=document.createElement('script');
     s.src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5427964157655690';
     s.async=true;
     s.crossOrigin='anonymous';
     document.head.appendChild(s);
   }
-  if(window.requestIdleCallback){
-    requestIdleCallback(loadAds);
-  }else{
-    window.addEventListener('load', loadAds);
+  function onUserInteraction(){
+    loadAds();
+    window.removeEventListener('scroll', onUserInteraction);
+    window.removeEventListener('mousemove', onUserInteraction);
+    window.removeEventListener('touchstart', onUserInteraction);
   }
+  window.addEventListener('scroll', onUserInteraction, {once:true});
+  window.addEventListener('mousemove', onUserInteraction, {once:true});
+  window.addEventListener('touchstart', onUserInteraction, {once:true});
 })();
